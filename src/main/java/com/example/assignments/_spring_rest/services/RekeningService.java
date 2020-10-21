d1704556f47dde60531336425b39282e2856a7a9
Jurian Vink <jurianvnk@gmail.com>
2020-10-21 23:17:55 +0200

    measured added for db calls

--- src/main/java/com/example/assignments/_spring_rest/services/RekeningService.java
+++ src/main/java/com/example/assignments/_spring_rest/services/RekeningService.java
@@ -1,5 +1,6 @@
 package com.example.assignments._spring_rest.services;
 
+import com.example.assignments._spring_rest.annotations.Measured;
 import com.example.assignments._spring_rest.models.Rekening;
 import com.example.assignments._spring_rest.repositories.RekeningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,23 +15,28 @@ public class RekeningService {
     @Autowired
     private RekeningRepository rekeningRepository;
 
+    @Measured(message = "Get rekeningen database call")
     public List<Rekening> getRekeningen() {
         return rekeningRepository.findAll();
     }
 
     @Cacheable("rekeningen")
+    @Measured(message = "Get rekening database call")
     public Optional<Rekening> getRekening(Long id) {
         return rekeningRepository.findById(id);
     }
 
+    @Measured(message = "Add rekening database call")
     public Rekening addRekening(Rekening rekening) {
         return rekeningRepository.save(rekening);
     }
 
+    @Measured(message = "Update rekening database call")
     public Rekening updateRekening(Rekening rekening) {
         return rekeningRepository.save(rekening);
     }
 
+    @Measured(message = "Remove rekening database call")
     public void removeRekening(Long rekeningId) {
         rekeningRepository.deleteById(rekeningId);
     }
